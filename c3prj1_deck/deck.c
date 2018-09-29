48c77401de86e7e377851caab6fde62cfea99874
Coursera Learner <nobody@nowhere.nul>
2018-09-29 09:37:15 +0000

    s

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -16,14 +16,11 @@ card_t card_from_num(unsigned c);
 
 void print_hand(deck_t * hand) {
   suit_t n = (*hand).n_cards;
+  card_t ** ptc1 = (*hand).cards;
   for(int i = 0; i < n; i++) {
-    if (i == n-1) {
-      card_t * ptc = *((*hand).cards)+i;
-      print_card(*ptc);
-    }
-    card_t * ptc1 = *((*hand).cards)+i;
-    print_card(*ptc1);
+    print_card(**ptc1);
     printf(" ");
+    ptc1++;
   }
 }
 
@@ -34,14 +31,14 @@ int deck_contains(deck_t * d, card_t c) {
   size_t b1;
 
   unsigned a2;
-  unsigned b2;
+  size_t b2;
 
   for(int i = 0; i < n; i++) {
-    a1 = (*(*((*d).cards)+i)).value;
-    b1 = (*(*((*d).cards)+i)).suit;
+    a1 = (**((*d).cards+i)).value;
+    b1 = (**((*d).cards+i)).suit;
     a2 = c.value;
     b2 = c.suit;
-    if (a1 == a2 && b1 == b2) {
+    if ((a1 == a2) && (b1 == b2)) {
       return 1;
     }
   }
@@ -50,13 +47,12 @@ int deck_contains(deck_t * d, card_t c) {
 
 void shuffle(deck_t * d){
   suit_t n = (*d).n_cards;
-  card_t * ptc = *((*d).cards);
+  card_t ** ptc = (*d).cards;
   card_t * arr[n];
   for (int j = 0; j < n; j++) {
-    arr[j] = ptc + j;
+    arr[j] = *(ptc + j);
   }
-  //use a different seed value so that we don't get same result each time we run this program
-  // srand(time(NULL));
+  
   for(int i = n-1; i > 0; i--) {
     int k = random() % (i+1);
     card_t * temp = arr[i];
@@ -67,17 +63,15 @@ void shuffle(deck_t * d){
     
 
 void assert_full_deck(deck_t * d) {
+
   suit_t n = (*d).n_cards;
-  card_t * ptc = *((*d).cards);
-  for (int i = n-1; i > 0; i--) {
-    deck_t new;
-    new.n_cards = n-i;
-    *(new.cards) = ptc + n - i;
-    deck_t * d1;
-    d1 = &new;
-    card_t c = *(ptc + n - 1 - i);
-    
-    int e = deck_contains(d1,c);
-    assert(e == 1);
+  card_t ** ptc = (*d).cards;
+  for (int i = 0; i < n; i++) {
+    card_t ** new = ptc+i;
+    (*d).cards = ptc+i+1;
+    int e = deck_contains(d,**new);
+    assert (e==0);
+
   }
+
 }
