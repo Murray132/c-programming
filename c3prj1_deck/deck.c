8883a671a18926ee4ef6aa2535e9cf706070a7cc
Coursera Learner <nobody@nowhere.nul>
2018-12-20 03:23:16 +0000

    s

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -34,8 +34,8 @@ int deck_contains(deck_t * d, card_t c) {
   size_t b2 = 0;
 
   for(int i = 0; i < n; i++) {
-    a1 = (**((*d).cards+i)).value;
-    b1 = (**((*d).cards+i)).suit;
+    a1 = (*(*((*d).cards+i))).value;
+    b1 = (*(*((*d).cards+i))).suit;
     a2 = c.value;
     b2 = c.suit;
     if ((a1 == a2) && (b1 == b2)) {
@@ -46,21 +46,28 @@ int deck_contains(deck_t * d, card_t c) {
 }
 
 void shuffle(deck_t * d){
-  size_t n = (*d).n_cards;
-  card_t ** ptc = (*d).cards;
-  card_t ** ptc1=ptc;
-  card_t ** ptc2=ptc;
-  for(int i = 0; i <n-1; i++){
+
+
+  //size_t n = (*d).n_cards;
+  //card_t ** ptc = (*d).cards;
   
-    int k = random()%(n-i);
-    ptc1 = ptc +i;
-    ptc2 = ptc +k;
+  //card_t ** ptc1=ptc;
+  //card_t ** ptc2=ptc;
+  //card_t * temp = *ptc1;
+  //*ptc1 = *ptc2;
+  //*ptc2 =temp;
+  
+  //for(int i = 0; i <n-1; i++){
+  
+  //int k = random()%(n-i);
+  //ptc1 = ptc +i;
+  //ptc2 = ptc +k;
  
-    card_t * temp = *ptc1;
-    *ptc1 = *ptc2;
-    *ptc2 = temp;
-    }
-  
+  //card_t * temp = *ptc1;
+    //*ptc1 = *ptc2;
+    //*ptc2 = temp;
+  //    }
+
 }
     
 
